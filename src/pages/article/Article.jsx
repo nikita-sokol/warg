392aa0c5b6a9bbfdfd2fe7dd8977dade01fa6099
Nikita Sokolov <snn0389@gmail.com>
2022-07-27 17:34:48 +0400

    article: finish

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -5,9 +5,32 @@ const Article = () => {
 
 
    return (
-      <div className='page container'>
+      <div className='container'>
          <div className="back"><Link to="/">На главную</Link></div>
-         Article
+         <article className='article'>
+            <h3 className='article-header'>Почему не рассматриваются заявки с жалобами на нарушение Правил игры?</h3>
+            <p className='article-content'>
+               <p className='article-text'>После введения функционала «Пожаловаться» не рассматриваются заявки, созданные в Центр поддержки пользователей о нарушении Правил игры по следующим пунктам:</p>
+               <h4 className='article-numbered-header'>«Оскорбления и провокации»</h4>
+               В данный пункт входит:
+               <ul className='article-list'>
+                  <li className='article-item'>использование ненормативной лексики и оскорбление собеседников, применение унизительных кличек и прозвищ по расовому, национальному, религиозному либо половому признаку, а также провоцирование собеседника к их использованию;</li>
+                  <li className='article-item'>клевета, размещение заведомо ложной информации об Игре, Администрации, Модераторах или Пользователях;</li>
+                  <li className='article-item'>угрозы реальной расправы в отношенииили представителей Администрации Проекта. Все виды соглашения.</li>
+               </ul>
+               <h4 className='article-numbered-header'>«Неинформативные сообщения»</h4>
+               В данный пункт входит:
+               <ul className='article-list'>
+                  <li className='article-item'>размещение информации (сообщения, фразы, символов и пр.), направленной на отвлечение Пользователей от темы беседы (флуд);</li>
+                  <li className='article-item'>все виды рекламных сообщений, в частности ссылки на интернет-ресурсы.</li>
+               </ul>
+               <p className='article-text'>Также не рассматриваются следующие заявки, созданные в Центр поддержки пользователей о нарушении Правил игры:</p>
+               <h4 className='article-numbered-header'>«Раскрытие позиций союзников»</h4>
+               <h4 className='article-numbered-header'>«Выталкивание союзника»</h4>
+               <h4 className='article-numbered-header'>«Неспортивное поведение»</h4>
+               <h4 className='article-numbered-header'>«Повреждение союзников»</h4>
+            </p>
+         </article>
       </div>
    );
 };
